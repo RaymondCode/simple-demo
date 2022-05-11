02a7f7006e7db6188cfca9855dc9482108105075
Raymond <raymondcode@outlook.com>
2022-05-11 17:00:38 +0800

    update post form request

--- controller/publish.go
+++ controller/publish.go
@@ -14,7 +14,7 @@ type VideoListResponse struct {
 
 // Publish check token then save upload file to public directory
 func Publish(c *gin.Context) {
-	token := c.Query("token")
+	token := c.PostForm("token")
 
 	if _, exist := usersLoginInfo[token]; !exist {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
