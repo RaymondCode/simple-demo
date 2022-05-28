b99614c91330f22bf184e0ac15d122ab3e41ee94
crx892476808 <892476808@qq.com>
2022-05-28 22:12:11 +0800

    新增video数据，可用于测试feed实现正确性

--- douyin.sql
+++ douyin.sql
@@ -92,5 +92,5 @@ CREATE TABLE `video_info`  (
 -- ----------------------------
 INSERT INTO `video_info` VALUES (1, '2', 'https://www.w3schools.com/html/movie.mp4', 'https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg', 1, 0, '测试1', '2022-05-28 09:43:37');
 INSERT INTO `video_info` VALUES (2, '1', 'https://www.w3schools.com/html/mov_bbb.mp4', 'https://www.topgoer.cn/uploads/blog/202111/attach_16b79b08788038a8.jpg', 1, 0, '测试2', '2022-05-28 09:43:45');
-
+INSERT INTO `video_info` VALUES (3,'3','http://rclc6mkkn.hn-bkt.clouddn.com/mikasa.mp4','http://rclc6mkkn.hn-bkt.clouddn.com/%E5%B0%8F%E4%B8%89%E7%AC%A0.jpg',3,0,'测试3','2022-05-28 22:00:00');
 SET FOREIGN_KEY_CHECKS = 1;
