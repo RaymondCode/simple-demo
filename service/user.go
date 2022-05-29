e7762184fc56b439534204c52277157e18311a56
jining <862217083@qq.com>
2022-05-29 23:22:42 +0800

    完成用户注册功能，请忽略go.sum的更新，随便覆盖他-张冀宁

--- service/user.go
+++ service/user.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/RaymondCode/simple-demo/global"
 	"github.com/RaymondCode/simple-demo/model"
 	"github.com/RaymondCode/simple-demo/repository"
 )
@@ -13,4 +14,25 @@ func (us *UserService) QueryUser(userId int64, token string) (*model.User, error
 		return nil, err
 	}
 	return rawUser, nil
+}
+
+// QueryUserByNameAndPassword 根据用户名和密码查询用户，返回用户id。
+func (us *UserService) QueryUserByNameAndPassword(username, password string) (int64, error) {
+	var user model.User
+	err := global.DB.Where("name = ? and password = ?", username, password).First(&user).Error
+	if err != nil {
+		return 0, err
+	}
+	return user.Id, nil
+}
+
+// IfNameExist 检查用户名是不是用过了。用过了返回1，没用过返回0。int具有比boolean更好的扩展性。
+func (us *UserService) IfNameExist(name string) int64 {
+	return repository.GroupApp.UserRepository.IfNameExist(name)
+}
+
+// SaveUser 把新创建的User存到数据库中
+func (us *UserService) SaveUser(username, password, nickname string) int64 {
+	user := model.User{Name: username, Password: password, Nickname: nickname}
+	return repository.GroupApp.UserRepository.SaveUser(&user)
 }
