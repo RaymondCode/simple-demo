8e8af854525db4ee2cbdd72df6445cc90b04d4b2
BaiZe1998 <1157467179@qq.com>
2022-06-05 18:26:47 +0800

    增加follow的单元测试

--- db/model/follow.go
+++ db/model/follow.go
@@ -16,8 +16,8 @@ type Follow struct {
 }
 
 // CreateFollow create follow info
-func CreateFollow(ctx context.Context, follows []*Follow) error {
-	if err := DB.WithContext(ctx).Create(follows).Error; err != nil {
+func CreateFollow(ctx context.Context, follow *Follow) error {
+	if err := DB.Table("follow").WithContext(ctx).Create(follow).Error; err != nil {
 		return err
 	}
 	return nil
@@ -29,13 +29,13 @@ func UpdateFollow(ctx context.Context, userID, followedUser uint, status *int) e
 	if status != nil {
 		params["status"] = *status
 	}
-	return DB.WithContext(ctx).Model(&Follow{}).Where("user_id = ? and followed_user = ?", userID, followedUser).
+	return DB.Table("follow").WithContext(ctx).Model(&Follow{}).Where("user_id = ? and followed_user = ?", userID, followedUser).
 		Updates(params).Error
 }
 
 // DeleteFollow delete follow info
 func DeleteFollow(ctx context.Context, userID uint, followedUser uint) error {
-	return DB.WithContext(ctx).Where("user_id = ? and followed_user = ? ", userID, followedUser).Delete(&Follow{}).Error
+	return DB.Table("follow").WithContext(ctx).Where("user_id = ? and followed_user = ? ", userID, followedUser).Delete(&Follow{}).Error
 }
 
 // QueryNote query list of note info
@@ -45,9 +45,9 @@ func QueryFollow(ctx context.Context, userID int64, status, limit, offset int) (
 	var conn *gorm.DB
 	// query for followed users
 	if status == 1 {
-		conn = DB.WithContext(ctx).Model(&Follow{}).Where("user_id = ?", userID)
+		conn = DB.Table("follow").WithContext(ctx).Model(&Follow{}).Where("user_id = ?", userID)
 	} else { // query for followers
-		conn = DB.WithContext(ctx).Model(&Follow{}).Where("followed_user = ?", userID)
+		conn = DB.Table("follow").WithContext(ctx).Model(&Follow{}).Where("followed_user = ?", userID)
 	}
 	if err := conn.Count(&total).Error; err != nil {
 		return res, total, err
