fe855d75c695ce2827c2a9124d4547eba984b2b1
nvmotou <1617738321@qq.com>
2022-06-09 00:15:43 +0800

    去掉无用的表名常量

--- common/common.go
+++ common/common.go
@@ -1,14 +1,6 @@
 package common
 
 const (
-	UserInfoTable    = "t_douyin_users"
-	VideoInfoTable   = "t_douyin_videos"
-	LikeInfoTable    = "t_douyin_favourites"
-	FollowInfoTable  = "t_douyin_follows"
-	CommentInfoTable = "t_douyin_comments"
-)
-
-const (
 	UserId   = "user_id"
 	Username = "user_name"
 )
