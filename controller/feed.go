9090529c2388218b23574c178c87d055e7940b90
钱锦辉 <qjh@qianjinhuideMacBook-Air.local>
2022-06-10 13:47:45 +0800

    添加评论、删除评论、修改了feed流的返回结果

--- controller/feed.go
+++ controller/feed.go
@@ -1,22 +1,25 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type FeedResponse struct {
 	Response
-	VideoList []Video `json:"video_list,omitempty"`
-	NextTime  int64   `json:"next_time,omitempty"`
+	VideoList    []Video `json:"video_list,omitempty"`
+	NextTime     int64   `json:"next_time,omitempty"`
+	CommentCount int64   `json:"comment_count,omitempty"`
 }
 
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
 	c.JSON(http.StatusOK, FeedResponse{
-		Response:  Response{StatusCode: 0},
+		Response: Response{StatusCode: 0,
+			StatusMsg: "",
+			NextTime:  time.Now().Unix()},
 		VideoList: DemoVideos,
-		NextTime:  time.Now().Unix(),
 	})
 }
