bbab2c3053e7020ff47a5dba614a7e9ce8d75f17
TremblingV5 <Administrator@XinZF>
2022-06-10 23:03:01 +0800

    添加了基于go-redis的redis封装

--- pkg/constants/constant.go
+++ pkg/constants/constant.go
@@ -2,5 +2,14 @@ package constants
 
 const (
 	MySQLDefaultDSN = "dyweadmin:JXU2MTNGJXU5NzUyJXU5Rjk5JXU2MzA3JXU1RjE1JXU0RjYw@tcp(xinzf0520.top:20607)/douyin?charset=utf8&parseTime=True&loc=Local"
-	MySQLLocalDSN   = "root:rootroot@tcp(127.0.0.1:3306)/douyin?charset=utf8&parseTime=True&loc=Local"
+	MySQLLocalDSN   = "root:root@tcp(localhost:3306)/douyin?charset=utf8&parseTime=True&loc=Local"
+
+	RedisLocalDSN   = "localhost:6379"
+	RedisLocalPWD   = ""
+	RedisDefaultDSN = "localhost:6379"
+	RedisDefaultPWD = ""
 )
+
+var RedisDBList = map[string]int{
+	"Default": 0,
+}
