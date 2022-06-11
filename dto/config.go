ef2e1cab1a13f3514319a847c39e10373a874902
TremblingV5 <Administrator@XinZF>
2022-06-11 11:17:35 +0800

    添加了配置文件，通过config目录下的config.yml进行配置

---
+++ dto/config.go
@@ -0,0 +1,68 @@
+package dto
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Env struct {
+		IsDebug bool `yaml:"debug"`
+	} `yaml:"env"`
+	Server struct {
+		Port string `yaml:"port"`
+	} `yaml:"server"`
+	MySQL struct {
+		Local struct {
+			Host     string `yaml:"host"`
+			Port     string `yaml:"port"`
+			Database string `yaml:"database"`
+			Username string `yaml:"username"`
+			Password string `yaml:"password"`
+		} `yaml:"local"`
+		Default struct {
+			Host     string `yaml:"host"`
+			Port     string `yaml:"port"`
+			Database string `yaml:"database"`
+			Username string `yaml:"username"`
+			Password string `yaml:"password"`
+		} `yaml:"default"`
+	} `yaml:"mysql"`
+	Redis struct {
+		Local struct {
+			Host     string `yaml:"host"`
+			Port     string `yaml:"port"`
+			Password string `yaml:"password"`
+		} `yaml:"local"`
+		Default struct {
+			Host     string `yaml:"host"`
+			Port     string `yaml:"port"`
+			Password string `yaml:"password"`
+		} `yaml:"default"`
+		Databases map[string]int `yaml:"databases"`
+	} `yaml:"redis"`
+}
+
+var cfg *Config
+
+func GetConfig() *Config {
+	return cfg
+}
+
+func InitConfig() error {
+	var config Config
+
+	file, err := ioutil.ReadFile("config/config.yml")
+
+	if err != nil {
+		return err
+	}
+
+	if err = yaml.Unmarshal(file, &config); err != nil {
+		return err
+	}
+
+	cfg = &config
+	return nil
+}
