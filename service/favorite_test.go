1d45d9562bccd1763fd3c90778c0ec180f86e08b
BaiZe1998 <1157467179@qq.com>
2022-06-11 23:26:41 +0800

    修改配置文件

--- service/favorite_test.go
+++ service/favorite_test.go
@@ -4,12 +4,11 @@ import (
 	"context"
 	"github.com/BaiZe1998/douyin-simple-demo/db"
 	"github.com/BaiZe1998/douyin-simple-demo/dto"
-	"github.com/BaiZe1998/douyin-simple-demo/service"
 	"testing"
 )
 
 func TestFavorite(t *testing.T) {
-	dto.InitConfigForTest()
+	dto.InitConfig()
 	db.Init()
-	service.GetFavoriteList(context.Background(), 5)
+	GetFavoriteList(context.Background(), 5)
 }
