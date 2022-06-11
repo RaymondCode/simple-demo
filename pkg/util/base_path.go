c81601adf017539fa2ffac0544b083f72eedfc6c
TremblingV5 <Administrator@XinZF>
2022-06-11 23:07:06 +0800

    更改了test文件存放的目录，更改了获取配置文件路径的方式，需要在Mac以及Linux系统中测试一下是否可用

--- pkg/util/base_path.go
+++ pkg/util/base_path.go
@@ -1,39 +1,16 @@
 package util
 
 import (
-	"log"
 	"os"
-	"path"
-	"path/filepath"
-	"runtime"
 	"strings"
 )
 
-func GetCurrentAbPath() string {
-	dir := getCurrentAbPathByExecutable()
-	tmpDir, _ := filepath.EvalSymlinks(os.TempDir())
-	if strings.Contains(dir, tmpDir) {
-		return getCurrentAbPathByCaller()
-	}
-	return dir
-}
+func GetConfigPath() string {
+	execFilePath := os.Args[0]
 
-// go build
-func getCurrentAbPathByExecutable() string {
-	exePath, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
+	if strings.Contains(execFilePath, ".test") {
+		return "../config/config.yml"
+	} else {
+		return "config/config.yml"
 	}
-	res, _ := filepath.EvalSymlinks(filepath.Dir(exePath))
-	return res
-}
-
-// go run
-func getCurrentAbPathByCaller() string {
-	var abPath string
-	_, filename, _, ok := runtime.Caller(0)
-	if ok {
-		abPath = path.Dir(filename)
-	}
-	return abPath
 }
