4507303f9cc27d12c6890f57655925e35e6a3256
BaiZe1998 <1157467179@qq.com>
2022-06-12 18:11:34 +0800

    redis完成封装

--- db/cache.go
+++ db/cache.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	"github.com/BaiZe1998/douyin-simple-demo/dto"
@@ -23,13 +24,13 @@ func InitRedisPools() error {
 		RedisPWD = dto.Conf.Redis.Default.Password
 	}
 
-	RedisCaches := make(map[string]*redis.Client)
+	RedisCaches = make(map[string]*redis.Client)
 	for k, v := range dto.Conf.Redis.Databases {
 		RedisCaches[k] = redis.NewClient(&redis.Options{
 			Addr:     RedisDSN,
 			Password: RedisPWD,
 			DB:       v,
-			PoolSize: 0,
+			PoolSize: 20,
 		})
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
@@ -52,4 +53,20 @@ func CacheSet(ctx context.Context, dbName string, key string, value string, expi
 func CacheGet(ctx context.Context, dbName string, key string) (string, error) {
 	result, err := RedisCaches[dbName].Get(ctx, key).Result()
 	return result, err
+}
+
+func CacheSetList(ctx context.Context, dbName string, key string, value interface{}, expire uint) error {
+	var valueJson []byte
+	valueJson, _ = json.Marshal(value)
+	if err := CacheSet(ctx, dbName, key, string(valueJson), expire); err != nil {
+		return err
+	}
+	return nil
+}
+
+func CacheGetList[T any](ctx context.Context, dbName string, key string, fix []T) ([]T, error) {
+	result, err := CacheGet(ctx, dbName, key)
+	var value []T
+	json.Unmarshal([]byte(result), &value)
+	return value, err
 }
