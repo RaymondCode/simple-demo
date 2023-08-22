c720ba17aff826b80536eec148a22242479dd651
yunyin <yunyin_jayyi@qq.com>
2023-08-23 00:19:14 +0800

    完成feed接口

--- Go-Project/controller/feed.go
+++ Go-Project/controller/feed.go
@@ -2,8 +2,8 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/life-studied/douyin-simple/service"
 	"net/http"
-	"time"
 )
 
 type FeedResponse struct {
@@ -14,9 +14,24 @@ type FeedResponse struct {
 
 // Feed same demo video list for every request
 func Feed(c *gin.Context) {
-	c.JSON(http.StatusOK, FeedResponse{
-		Response:  Response{StatusCode: 0},
-		VideoList: DemoVideos,
-		NextTime:  time.Now().Unix(),
+	strLatestTime, exists := c.GetQuery("latest_time")
+	if !exists {
+		c.JSON(http.StatusBadRequest, FeedResponse{Response: Response{StatusCode: 1, StatusMsg: "latest_time is empty"}})
+		return
+	}
+	nextTime, startId, err := service.GetNextTime(strLatestTime)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, FeedResponse{Response: Response{StatusCode: 1, StatusMsg: err.Error()}})
+		return
+	}
+	var endId int64
+	if startId+30 > int64(len(DemoVideos)) {
+		endId = int64(len(DemoVideos))
+	} else {
+		endId = startId + 30
+	}
+	c.JSON(http.StatusOK, FeedResponse{Response: Response{StatusCode: 0},
+		VideoList: DemoVideos[startId:endId],
+		NextTime:  nextTime,
 	})
 }
