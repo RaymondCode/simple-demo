90eaeeb41ebfa1b1da72b4a07714361ef288ce6c
46135621 <985579956@qq.com>
2023-08-24 15:59:40 +0800

    rpc.video配置数据库

--- go.mod
+++ go.mod
@@ -17,10 +17,13 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -48,5 +51,7 @@ require (
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.1 // indirect
+	gorm.io/gorm v1.25.4 // indirect
 	moul.io/http2curl/v2 v2.3.0 // indirect
 )
--- service/rpc/video/internal/svc/serviceContext.go
+++ service/rpc/video/internal/svc/serviceContext.go
@@ -1,13 +1,46 @@
 package svc
 
-import "github.com/RaymondCode/simple-demo/service/rpc/video/internal/config"
+import (
+	"fmt"
+	"github.com/RaymondCode/simple-demo/common/model"
+	"github.com/RaymondCode/simple-demo/service/rpc/video/internal/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	Mysql  *gorm.DB
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		Mysql:  initMysql(c),
 	}
+}
+func initMysql(c config.Config) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.Mysql.Username,
+		c.Mysql.Password,
+		c.Mysql.Address,
+		c.Mysql.DBName,
+	)
+	//root:SXFsvsxegJ4PtI84MR@tcp(ts.dn11.top:33066)/tiktok-video?charset=utf8mb4&parseTime=True&loc=Local
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   c.Mysql.TablePrefix, // 表名前缀
+			SingularTable: true,                // 使用单数表名
+		},
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&model.Video{}, &model.Favorite{}, &model.Comment{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
