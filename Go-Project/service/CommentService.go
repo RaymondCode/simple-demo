fa8c90c3386a8cac388cf54cdfc67ac568f62a2f
CoMet.C <136603951+c1184387860@users.noreply.github.com>
2023-08-29 18:29:27 +0800

    改进comment使其能通过测试 (#23)

--- Go-Project/service/CommentService.go
+++ Go-Project/service/CommentService.go
@@ -1,13 +1,10 @@
 package service
 
 import (
-	"errors"
 	"fmt"
 	"github.com/life-studied/douyin-simple/dao"
-	"github.com/life-studied/douyin-simple/global"
 	"github.com/life-studied/douyin-simple/model"
 	"github.com/life-studied/douyin-simple/response"
-	"gorm.io/gorm"
 	"net/http"
 	"time"
 )
@@ -23,18 +20,6 @@ func IntTime2StrTime(intTime int64) string {
 	return time.Unix(intTime, 0).Format(template)
 }
 
-// GetFollowByUserId 检查user1是否follow了user2
-func GetFollowByUserId(userId1 int64, userId2 int64) bool {
-	relationship := model.Relation{}
-	if userId1 == userId2 || userId1 == 0 {
-		return false
-	}
-	if err := global.DB.Model(&model.Relation{}).Where("host_id=? And guest_id=?", userId1, userId2).First(&relationship).Error; err == gorm.ErrRecordNotFound {
-		return false
-	}
-	return true
-}
-
 func CreateComment(userID int64, videoID int64, commentText string) (response.CommentActionResponse, error) {
 	// 获取评论时间
 	currentTime := time.Now().Unix()
@@ -44,7 +29,7 @@ func CreateComment(userID int64, videoID int64, commentText string) (response.Co
 		UserId:     userID,
 		VideoId:    videoID,
 		Content:    commentText,
-		CreateDate: currentTime,
+		CreateDate: IntTime2StrTime(currentTime),
 	}
 	//  将comment增添到数据库中
 	tx := dao.BeginTransaction()
@@ -90,16 +75,13 @@ func CreateComment(userID int64, videoID int64, commentText string) (response.Co
 	// 返回响应
 	commentActionResponse := response.CommentActionResponse{
 		Response: response.Response{StatusCode: 0, StatusMsg: "OK"},
-		Comment: response.Comment_Response{
+		Comment: model.Comment{
 			ID:         comment.ID,
 			Content:    comment.Content,
 			CreateDate: createDate,
-			Userresponse: response.User_Response{
-				ID:            uint(commenter.Id),
-				Name:          commenter.Name,
-				FollowCount:   uint(commenter.FollowCount),
-				FollowerCount: uint(commenter.FollowerCount),
-				IsFollow:      false, // 待确定自己与自己的关注状态
+			User: model.User{
+				Id:   commenter.Id,
+				Name: commenter.Name,
 			},
 		},
 	}
@@ -154,59 +136,26 @@ func DeleteComment(userID int64, videoID int64, commentID int64) (response.Comme
 
 		commentActionResponse = response.CommentActionResponse{
 			Response: response.Response{StatusCode: 0, StatusMsg: "删除成功"},
-			Comment:  response.Comment_Response{},
+			Comment:  model.Comment{},
 		}
 
 	} else {
 		commentActionResponse = response.CommentActionResponse{
 			Response: response.Response{StatusCode: 0, StatusMsg: "无删除权限"},
-			Comment:  response.Comment_Response{},
+			Comment:  model.Comment{},
 		}
 	}
 	return commentActionResponse, err
 }
 
-func GetCommentList(videoID int64, userID int64) ([]response.Comment_Response, error) {
+func GetCommentList(videoID int64) ([]model.Comment, error) {
 
-	// 从数据库中获取id为video_id的全部评论
-	comments, err := dao.GetCommentByIdListById(videoID)
+	var rawComments []model.Comment
+	var err error
+	rawComments, err = dao.QueryCommentsByVideoId(videoID)
 	if err != nil {
-		err = errors.New("视频数据错误")
 		return nil, err
 	}
+	return rawComments, nil
 
-	// 将获取到的评论添加到commentList列表中
-	// 将model.comment解析为response.Comment_Response格式
-	// 将获取到的评论添加到commentList列表中
-	var commentList []response.Comment_Response
-	// 将model.comment解析为response.Comment_Response格式
-	for _, comment := range comments {
-
-		// 根据评论者id构建user_response
-		commenter, err := dao.GetUserById(comment.UserId)
-		if err != nil {
-			// 处理获取用户信息错误
-			continue // 继续处理下一个评论
-		}
-
-		// 构建Comment_Response中嵌套的User_Response字段
-		userResponse := response.User_Response{
-			ID:            uint(commenter.Id),
-			FollowCount:   uint(commenter.FollowCount),
-			FollowerCount: uint(commenter.FollowerCount),
-			Name:          commenter.Name,
-		}
-		//查询该用户是否被关注
-
-		userResponse.IsFollow = GetFollowByUserId(userID, commenter.Id)
-
-		commentResponse := response.Comment_Response{
-			ID:           uint(comment.Id),
-			Content:      comment.Content,
-			CreateDate:   IntTime2StrTime(comment.CreateDate),
-			Userresponse: userResponse,
-		}
-		commentList = append(commentList, commentResponse)
-	}
-	return commentList, err
 }
